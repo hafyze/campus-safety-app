5f3f12de7ac32fc7b1cdabdb4dfa803854b68cfc
hyefyze <hafizmatqwer@gmail.com>
2025-08-24 12:03:45 +0800

    update: modify .gitignore to include www/ and switch adapter in svelte.config.js to @sveltejs/adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		fallback: 'index.html'
-	}) }
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html' // so SPA routing works
+    })
+  }
 };
 
 export default config;
