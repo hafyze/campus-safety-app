23ea2a96e0dd45bbefd51569a36a807a091a0675
hyefyze <hafizmatqwer@gmail.com>
2025-08-24 12:12:31 +0800

    fix: edit .json file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>COOKING HACKATHON CODENECTION 2025</h1>
+<script lang="ts">
+	import { Geolocation } from '@capacitor/geolocation';
+
+	let loc:any = null;
+	async function getCurrentPosition() {
+		const res = await Geolocation.getCurrentPosition();
+		loc = res;
+	}
+</script>
+
+<div>
+	<h1>Geolocation</h1>
+	<p>Your location is:</p>
+	<p>Latitude: {loc?.coords.latitude}</p>
+	<p>Longitude: {loc?.coords.longitude}</p>
+
+	<button on:click={getCurrentPosition}> Get Current Location </button>
+</div>
